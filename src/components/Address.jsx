9c185d7224efe48da909191b340d403f56ff14a5
victorycineworld <it@victorycineworld.com>
2024-10-27 18:59:30 +0530

    Contact Updated

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -5,15 +5,18 @@ const Address = () => {
     <>
       <p className="open-sans-font custom-span-contact position-relative">
         <i className="fa fa-map position-absolute"></i>
-        <span className="d-block">Address Point</span>123 Stree New York City ,
-        United States Of America 750065.
+        <span className="d-block">Address Point</span>Victory Cineworld, Tapadia
+        Terrace, Adalat Rd, opposite Distric Court ,Kotla Colony, Chhatrapati
+        Sambhajinagar (Aurangabad), Maharashtra-431001
       </p>
       {/* End .custom-span-contact */}
 
       <p className="open-sans-font custom-span-contact position-relative">
         <i className="fa fa-envelope-open position-absolute"></i>
         <span className="d-block">mail me</span>{" "}
-        <a href="mailto:hello@victorycineworld.com">hello@victorycineworld.com</a>
+        <a href="mailto:hello@victorycineworld.com">
+          hello@victorycineworld.com
+        </a>
       </p>
       {/* End .custom-span-contact */}
 
@@ -21,6 +24,8 @@ const Address = () => {
         <i className="fa fa-phone-square position-absolute"></i>
         <span className="d-block">call me</span>{" "}
         <a href="Tel: +91 8459686970">+216 21 184 010</a>
+        <br/>
+        <a href="Tel: +91 8459686970">+91 9689 896 355</a>
       </p>
       {/* End .custom-span-contact */}
     </>
