9f44771cf542988cff6cc3a7a898ba00872deaa5
victorycineworld <it@victorycineworld.com>
2024-11-22 17:23:11 +0530

    Changes done in the blog section

--- src/Hooks/AllBlogData.js
+++ src/Hooks/AllBlogData.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import img1 from "../assets/img/blog/blog-post-1.jpg";
+import img1 from "../assets/blog1.png";
 import img2 from "../assets/img/blog/blog-post-2.jpg";
 import img3 from "../assets/img/blog/blog-post-3.jpg";
 import img4 from "../assets/img/blog/blog-post-4.jpg";
@@ -11,19 +11,17 @@ const AllBlogData = () => {
     {
       id: 1,
       img: img1,
-      title: "How to Own Your Audience by Creating an Email List",
-      commentor: "Rio ",
-      date: "21 April 2022",
-      tag: `wordpress, business, economy, design`,
+      title: "India Country Chair (IT services and GIG Economy)",
+      commentor: "INdia ",
+      date: "20 November 2024",
+      tag: `All Ladies League`,
       description1:
-        "Tomfoolery crikey bits and bobs brilliant bamboozled down the pub amongst brolly hanky panky, cack bonnet arse over tit burke bugger all mate bodge. cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Suspendisse interdum consectetur libero id faucibu nisl. Lacus vel facilisis volutpat est velit egestas.",
+        "Beyond thrilled to join the ranks of the extraordinary G100! A huge thank you to Paulina Chotrova, Dr. Harbeen Arora Rai and Anisha Tiwari for this incredible honor. ",
       description2:
-        "Most photographers find it hard to see interesting pictures in places in which they are most familiar. A trip somewhere new seems always exactly what our photography needed, as shooting away from home consistently inspires us to new artistic heights. ",
+        "A special thank you to Jaycy Naveen for her constant support and believing in my capabilities. ",
       description3:
-        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.",
-      description4:
-        "Riosum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    },
+        " Can't wait to work together with this amazing group of women leaders to drive positive change in India and around the world",
+       },
     {
       id: 2,
       img: img2,
